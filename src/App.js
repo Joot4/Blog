1707a476d9b214b95ad4a6461dfee549e907c5be
joota <jrtatay@hotmail.com>
2022-07-29 16:02:33 -0300

    structure new components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import Header from './components/Header';
+import Article from './components/Article';
+import Post from './components/Post';
 
 function App() {
   return (
+    <> 
+    <Article></Article>
+    <Header></Header>
+    <Post></Post>
     <h1>Meu blog</h1>
-  );
+    </>
+    );
 }
 
 export default App;
